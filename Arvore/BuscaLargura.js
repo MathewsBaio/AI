3e22085afc13485a57de92d3326260eeb8067dab
Mathews <baiomathews@gmail.com>
2025-02-20 21:46:39 -0300

    Busca em largura

---
+++ Arvore/BuscaLargura.js
@@ -0,0 +1,45 @@
+class BuscaLargura {
+  static porLargura(no, fila) {
+    if (!fila || fila.length == 0) {
+      return;
+    }
+
+    if (!no) {
+      return;
+    }
+
+    if (no.filhoEsquerda) {
+      fila.push(no.filhoEsquerda);
+    }
+
+    if (no.filhoDireita) {
+      fila.push(no.filhoDireita);
+    }
+
+    console.log(no);
+    fila.shift();
+    this.porLargura(fila[0], fila);
+  }
+
+  static buscaLargura(raiz) {
+    if (!raiz) {
+      return;
+    }
+
+    let fila = [raiz];
+    atual = null;
+    while (fila.length > 0) {
+      atual = fila.shift();
+
+      console.log(atual);
+
+      if (atual.filhoEsquerda) {
+        fila.push(atual.filhoEsquerda);
+      }
+
+      if (atual.filhoDireita) {
+        fila.push(atual.filhoDireita);
+      }
+    }
+  }
+}
--- Arvore/BuscaProfundidade.js
+++ Arvore/BuscaProfundidade.js
@@ -27,28 +27,6 @@ class BuscaProfundidade {
     this.posOrdem(no.filhoDireita);
     console.log(no.valor);
   }
-
-  static porLargura(no, fila) {
-    if (!fila || fila.length == 0) {
-      return;
-    }
-
-    if (!no) {
-      return;
-    }
-
-    if (no.filhoEsquerda) {
-      fila.push(no.filhoEsquerda);
-    }
-
-    if (no.filhoDireita) {
-      fila.push(no.filhoDireita);
-    }
-
-    console.log(no);
-    fila.shift();
-    this.porLargura(fila[0], fila);
-  }
 }
 
 export default BuscaProfundidade;
