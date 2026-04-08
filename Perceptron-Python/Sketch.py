2ab73d47c34c8a27c58b98a196abf124b859db21
Mathews Baio <baiomathews@gmail.com>
2026-04-08 20:16:16 -0300

    aula 08/04/2026

--- Perceptron-Python/Sketch.py
+++ Perceptron-Python/Sketch.py
@@ -1,4 +1,5 @@
 from Perceptron import Perceptron
+from Point import Point
 
 percep = Perceptron()
 
@@ -7,3 +8,7 @@ inputs = [-1,0.5]
 chute = percep.guess(inputs)
 
 print (f"O chute do perceptron foi: {chute}")
+
+
+ponto = Point(4,2)
+ponto.debug()
